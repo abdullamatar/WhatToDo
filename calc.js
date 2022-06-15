c1484169cab9dc63d740ac91efc58dc81ef7e536
Abdulla <abdullabloushi@gmail.com>
2022-06-15 13:44:27 +0400

    Added README, and other small changes

--- calc.js
+++ calc.js
@@ -1,11 +1,3 @@
-/*
-
-!script to display remaining days of expected lifespan...just to get you going 😁( ﾉ ﾟｰﾟ)ﾉ＼(ﾟｰﾟ＼)
-@params age, var expected, var remainingDays;
-
-*/
-// a = Number.parseInt(a);
-// e = Number.parseInt(e);
 function imReallyJustRushingThis(){
     var a = document.getElementById("age").value;
     var e = document.getElementById("expected").value;
