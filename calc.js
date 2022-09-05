7cfdf1053a552eaccf91b50b78a0e5b80609bb3d
Abdulla <abdullabloushi@gmail.com>
2022-09-05 21:03:49 +0400

    Testing changes

--- calc.js
+++ calc.js
@@ -1,8 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
 function imReallyJustRushingThis(){
     var a = document.getElementById("age").value;
     var e = document.getElementById("expected").value;
     var daysLeft = 0;
     a *= 365; e*=365;
     daysLeft = e-a;
-    document.getElementById("output").innerHTML = `Fortunately you've got around ${daysLeft} days left, what are you gonna do next? 🚀`;    
+    document.getElementById("output").innerHTML = `Fortunately you've got around ${daysLeft} days left, what are you gonna do next? 🚀`;
 }
