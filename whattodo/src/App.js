f56caeb4f360f2e4165f067fc03dbebb9300a630
Abdulla <abdullabloushi@gmail.com>
2022-10-02 16:08:13 +0400

    this is a git commit message

---
+++ whattodo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LifeExpectancy from './LifeExpectancy';
+
+function App() {
+  return (
+    <>
+  <LifeExpectancy/>
+  {/* <input type="text"/> */}
+    </>
+  );
+}
+
+export default App;
