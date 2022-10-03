5b86fc396d351a6b99c82fa2e0d568653b998d1d
Abdulla <axa1808@student.bham.ac.uk>
2022-10-03 16:49:04 +0400

    moved to create-react-app

--- whattodo/src/App.js
+++ whattodo/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import './App.css';
 import LifeExpectancy from './LifeExpectancy';
-
 function App() {
   return (
     <>
-  <LifeExpectancy/>
-  {/* <input type="text"/> */}
+    <LifeExpectancy/>
     </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
